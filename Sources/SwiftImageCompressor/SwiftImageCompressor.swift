72cb83af3f03d45a748a6a2af4c0e2654bc2d3e2
Kim JaeHyung <you3192001@icloud.com>
2025-06-29 18:30:05 +0900

    [UPDATE] Image Resizing Logic

--- Sources/SwiftImageCompressor/SwiftImageCompressor.swift
+++ Sources/SwiftImageCompressor/SwiftImageCompressor.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ImageIO
 
 public final class SwiftImageCompressor: Sendable {
     
@@ -24,7 +25,7 @@ public final class SwiftImageCompressor: Sendable {
         targetMB: Double,
         maxDimension: CGFloat = 2048
     ) -> Data? {
-        let resized = resizeImage(image, maxDimension: maxDimension)
+        let resized = downSample(image, maxDimension: maxDimension)
         
         switch type {
         case .jpeg:
@@ -52,7 +53,7 @@ public final class SwiftImageCompressor: Sendable {
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let self else { return }
             
-            let resized = self.resizeImage(image, maxDimension: maxDimension)
+            let resized = self.downSample(image, maxDimension: maxDimension)
             let result: Data?
 
             switch type {
@@ -83,7 +84,7 @@ public final class SwiftImageCompressor: Sendable {
     ) async -> Data? {
         await Task.detached(priority: .userInitiated) { [weak self] in
             guard let self else { return nil }
-            let resized = resizeImage(image, maxDimension: maxDimension)
+            let resized = downSample(image, maxDimension: maxDimension)
             
             switch type {
             case .jpeg:
@@ -107,6 +108,7 @@ extension SwiftImageCompressor {
     ///   - image: UIImage
     ///   - maxDimension: ex) 2048 -> 2048x2048
     /// - Returns: UIImage
+    @available(*, message: "This Function Will Be Deprecated Soon Use downsampling function")
     public func resizeImage(_ image: UIImage, maxDimension: CGFloat) -> UIImage {
         let width = image.size.width
         let height = image.size.height
@@ -128,10 +130,49 @@ extension SwiftImageCompressor {
         }
         
         let renderer = UIGraphicsImageRenderer(size: newSize)
+        
         return renderer.image { context in
             image.draw(in: CGRect(origin: .zero, size: newSize))
         }
     }
+}
+
+// MARK: DownSample
+extension SwiftImageCompressor {
+    
+    /// DownSample Func
+    /// - Parameters:
+    ///   - image: original Image
+    ///   - maxDimension: 2048x 2048 -> 2048
+    /// - Returns: DownSample Image or Fail To OriginalImage
+    func downSample(_ image: UIImage, maxDimension: CGFloat = 2048) -> UIImage {
+        guard let data = image.pngData() else {
+            print("Image Data Error")
+            return image
+        }
+        
+        let cfData = data as CFData
+        
+        guard let imageSource = CGImageSourceCreateWithData(cfData, nil) else {
+            print("Image Source Fail")
+            return image
+        }
+        
+        let maxPixel = maxDimension
+        
+        let options = [
+            kCGImageSourceThumbnailMaxPixelSize: maxPixel,
+            kCGImageSourceCreateThumbnailFromImageAlways: true
+        ] as CFDictionary
+        
+        guard let scaledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options) else {
+            print("Downsample Failed")
+            return image
+        }
+        
+        return UIImage(cgImage: scaledImage)
+    }
+
 }
 
 // MARK: Compresse
@@ -145,29 +186,37 @@ extension SwiftImageCompressor {
         var low: CGFloat = 0
         var high: CGFloat = 1
         let minQuality: CGFloat = 0.1
-        var bestData: Data? = from.jpegData(compressionQuality: 1)
+        var bestData: Data? = nil
         
+        
+        bestData = from.jpegData(compressionQuality: 1)
         if let bestData {
             let calc = calcMBSize(of: bestData)
             if calc <= mbSize {
+                print("already best")
                 return bestData
             }
         }
         
-        while high - low > 0.05 && high > minQuality {
-            let mid = (low + high) / 2
-            guard let data = from.jpegData(compressionQuality: mid) else {
-                break
+        autoreleasepool {
+            while high - low > 0.05 && high > minQuality {
+                let mid = (low + high) / 2
+                guard let data = from.jpegData(compressionQuality: mid) else {
+                    break
+                }
+                
+                let sizeMB = calcMBSize(of: data)
+                // ex) 150mb > 100mb
+                if sizeMB > mbSize {
+                    print("Not !!!")
+                    high = mid
+                } else {
+                    bestData = data
+                    print("BEST !!!")
+                    low = mid
+                }
             }
-            
-            let sizeMB = calcMBSize(of: data)
-            // ex) 150mb > 100mb
-            if sizeMB > mbSize {
-                high = mid
-            } else {
-                bestData = data
-                low = mid
-            }
+            print("결말")
         }
         
         return bestData
@@ -245,4 +294,10 @@ extension UIImage {
         return SwiftImageCompressor.shared.resizeImage(self, maxDimension: maxDimension)
     }
     
+}
+
+fileprivate final class TestUIImage: UIImage {
+    deinit {
+        print("DEAD")
+    }
 }
